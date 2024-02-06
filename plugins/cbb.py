ed9fe1f6c29dbc29d66d05acdc4be02f160f9af7
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-06 23:25:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,30 +16,47 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                         InlineKeyboardButton("⌬ My Owner ⌬", url = "https://t.me/Shidoteshika1")
                     ],[
                         InlineKeyboardButton('Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ', url = 'https://t.me/Animemoviesr'),
-InlineKeyboardButton('Aɴɪᴍᴇ Gʀᴏᴜᴘ', url = 'https://t.me/ChatBox480')
+                        InlineKeyboardButton('Oɴɢᴏɪɴɢ Aɴɪᴍᴇ', url = 'https://t.me/Infinity_Ongoing')
                     ],[
                         InlineKeyboardButton("⛔️ Close ⛔️", callback_data = "close")
                     ]])
         )
     elif data == "help":
            await query.message.edit_text(
-                        text = """<b><u>BOT COMMANDS:</u>
+                        text = """<b>○ <u>BOT COMMANDS</u> ○
 
-❏ Commands For BOT Admins
-├/start : start the bot or get posts
-├/batch : Create Group Message
-├/genlink : create link for one post
-├/users : view bot statistics
-├/broadcast : broadcast Message
-├/tutorial : To get a screen record
-└/stats : checking your bot uptime</b>""",
+❏ Cᴏᴍᴍᴀɴᴅs ғᴏʀ ʙᴏᴛ Aᴅᴍɪɴs
+
+‣ /start :</b> start the bot or get posts
+<b>‣ /batch :</b> create group messages
+<b>‣ /genlink :</b> create link for one post
+<b>‣ /users :</b> view bot statistics
+<b>‣ /broadcast :</b> broadcast Message
+<b>‣ /stats :</b> checking your bot uptime""",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("⬅️ Back", callback_data = "about"),
-                        InlineKeyboardButton("⛔️ Close", callback_data = "close")
-                    ]])
+                        InlineKeyboardButton("🤖 About", callback_data = "about"),
+                        InlineKeyboardButton("⬅️ Back", callback_data = "start")
+                    ],[
+                        InlineKeyboardButton("⛔️ Close ⛔️", callback_data = "close")
+                ]])
+        )
+    elif data == "start":
+        await query.message.edit_text(
+                    text = """Hᴇʟʟᴏ, {first} 👋\n\n<b>I ᴀᴍ Oɴʟʏ Sᴛᴏʀᴇ ғɪʟᴇs ғᴏʀ <a href='https://t.me/Animemoviesr'>infinity void ∞</a></b>""",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171')
+                ], [
+                     InlineKeyboardButton("🤖 About Me", callback_data = "about"),
+                     InlineKeyboardButton("❕Help", callback_data = "help")
+                ],[
+                     InlineKeyboardButton("⛔️ Close ⛔️", callback_data = "close")
+            ]])
         )
     elif data == "close":
         await query.message.delete()
