3a32e3eec1b854b3dbba628850a73f5e7adbf6e1
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-06 09:48:04 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -12,6 +12,8 @@ from pyrogram.errors import FloodWait
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
+    if not FORCE_SUB_CHANNEL1:
+        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
@@ -19,19 +21,6 @@ async def is_subscribed(filter, client, update):
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
     except UserNotParticipant:
         return False
-
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
-
-async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL1:
-        return True
-    user_id = update.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
         member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL1, user_id = user_id)
     except UserNotParticipant:
         return False
@@ -40,7 +29,6 @@ async def is_subscribed(filter, client, update):
         return False
     else:
         return True
-
 
 async def encode(string):
     string_bytes = string.encode("ascii")
