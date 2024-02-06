e97005ec078aa5ff4e6a4dacc24385ffd3e6e013
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-06 10:16:00 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,21 +10,30 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_CHANNEL1:
+    if not (FORCESUB_CHANNEL or FORCESUB_CHANNEL1):
         return True
+
     user_id = update.from_user.id
+
     if user_id in ADMINS:
         return True
-    try:
-       if member == await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id) and member == await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL1, user_id = user_id):
-            except UserNotParticipant:
+
+    member_status = ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER
+
+    for channel_id in [FORCESUB_CHANNEL, FORCESUB_CHANNEL1]:
+        if not channel_id:
+            continue
+
+        try:
+            member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        except UserNotParticipant:
             return False
 
-    if not member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]:
-        return False
-    else:
-        return True
+        if member.status not in member_status:
+            return False
 
+    return True
+    
 async def encode(string):
     string_bytes = string.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(string_bytes)
