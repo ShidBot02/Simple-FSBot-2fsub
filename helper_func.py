4945e2d7eee31f1aad66dd0180c53b7d3da0d43b
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-06 09:54:33 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -10,18 +10,13 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    if not FORCE_SUB_CHANNEL1:
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_CHANNEL1:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL1, user_id = user_id)
+        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, chat_id = FORCE_SUB_CHANNEL1, user_id = user_id)
     except UserNotParticipant:
         return False
 
