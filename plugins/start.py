50227af9a49d7fea8172f37546080ff5dec70167
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-10 00:08:31 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -111,31 +111,97 @@ async def start_command(client: Client, message: Message):
         return
 
 
-@Client.on_message(filters.private & filters.command("hey"))
-async def hey(client, message):
-  await message.edit_text(
-                        text = """<b>○ <u>BOT COMMANDS</u> ○
+@Bot.on_message(filters.command('hey') & filters.private & subscribed)
+async def hey(client: Client, message: Message):
+    id = message.from_user.id
+    if not await present_user(id):
+        try:
+            await add_user(id)
+        except:
+            pass
+    text = message.text
+    if len(text)>7:
+        try:
+            base64_string = text.split(" ", 1)[1]
+        except:
+            return
+        string = await decode(base64_string)
+        argument = string.split("-")
+        if len(argument) == 3:
+            try:
+                start = int(int(argument[1]) / abs(client.db_channel.id))
+                end = int(int(argument[2]) / abs(client.db_channel.id))
+            except:
+                return
+            if start <= end:
+                ids = range(start,end+1)
+            else:
+                ids = []
+                i = start
+                while True:
+                    ids.append(i)
+                    i -= 1
+                    if i < end:
+                        break
+        elif len(argument) == 2:
+            try:
+                ids = [int(int(argument[1]) / abs(client.db_channel.id))]
+            except:
+                return
+        temp_msg = await message.reply("Loading...")
+        try:
+            messages = await get_messages(client, ids)
+        except:
+            await message.reply_text("Something went wrong..!")
+            return
+        await temp_msg.delete()
 
-❏ Cᴏᴍᴍᴀɴᴅs ғᴏʀ ʙᴏᴛ Aᴅᴍɪɴs
+        for msg in messages:
 
-‣ /start :</b> start the bot or get posts
-<b>‣ /batch :</b> create group messages
-<b>‣ /genlink :</b> create link for one post
-<b>‣ /users :</b> view bot statistics
-<b>‣ /broadcast :</b> broadcast Message
-<b>‣ /stats :</b> checking your bot uptime
+            if bool(CUSTOM_CAPTION) & bool(msg.document):
+                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+            else:
+                caption = "" if not msg.caption else msg.caption.html
 
-<b>➪ For more Help Contact- @ChatBox480</b>""",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            if DISABLE_CHANNEL_BUTTON:
+                reply_markup = msg.reply_markup
+            else:
+                reply_markup = None
+
+            try:
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+            except:
+                pass
+        return
+    else:
+        reply_markup = InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton("🤖 About", callback_data = "about"),
-                        InlineKeyboardButton("⬅️ Back", callback_data = "start")
-                    ],[
-                        InlineKeyboardButton("⛔️ Close ⛔️", callback_data = "close")
-                ]])
+                    InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171')
+                ], [
+                     InlineKeyboardButton("🤖 About Me", callback_data = "about"),
+                     InlineKeyboardButton("❕Help", callback_data = "help")
+                ],[
+                     InlineKeyboardButton("⛔️ Close ⛔️", callback_data = "close")
+         ]])
+         
+        await message.reply_text(
+            text = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
         )
+        return
 
    
 #=====================================================================================##
