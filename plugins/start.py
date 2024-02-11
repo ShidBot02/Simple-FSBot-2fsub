88bccd63d57215a31e65cd870284d97b59a5b01d
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-11 23:50:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -95,8 +95,9 @@ async def start_command(client: Client, message: Message):
                      InlineKeyboardButton("❕", callback_data = "help")
                 ]])
          
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo= ONGOING,
+            caption= START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -104,7 +105,6 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
             quote = True
         )
         return
