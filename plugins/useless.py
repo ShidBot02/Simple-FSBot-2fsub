02ac74c033054617f6ec443f79f03eafc2a1e4e0
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-14 13:39:36 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,7 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, BOT_CMD
 from datetime import datetime
 from helper_func import get_readable_time
 
@@ -12,6 +12,9 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
+@Bot.on_message(filters.command('bcmd') & filters.user(ADMINS))
+async def bcmd(bot: Bot, message: Message):
+    await message.reply(BOT_CMD)
 
 @Bot.on_message(filters.private)
 async def useless(_,message: Message):
