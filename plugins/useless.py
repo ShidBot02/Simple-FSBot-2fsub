6a5ece4280e851a208b62ff4f55506d7a12f6f0e
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-02-14 13:59:34 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -17,7 +17,7 @@ async def stats(bot: Bot, message: Message):
 async def bcmd(bot: Bot, message: Message):
     await message.reply(BOT_CMD)
     await asyncio.sleep(30)
-    await query.message.delete()
+    await message.delete()
         
 
 @Bot.on_message(filters.private)
